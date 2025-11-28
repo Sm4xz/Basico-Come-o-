83ebcc7d6dc648b3451686b96e8887b23335b933
Sm4xz <mcpedasgalinha@gmail.com>
2025-11-28 11:07:13 -0300

    Adiciona JSON, Validação e Backup

--- hardware/ram.go
+++ hardware/ram.go
@@ -1,16 +1,17 @@
 package hardware
 
 type Memoria struct {
-	Marca      string
-	Frequencia int
-	Latencia   int
-	Voltagem   float64
+	Marca      string `json:"marca"`
+	Frequencia int    `json:"freq_mhz"`
+	Latencia   int    `json:"cas_latency"`
+	Voltagem   float64 `json:"voltagem"`
 }
 
 func (m Memoria) EhBdie() bool {
-	if m.Latencia <= 14 {
-		return true
-	} else {
-		return false
-	}
+	if m.Frequencia >= 3200 && m.Latencia <=14  {
+		return true }
+    if m.Frequencia >= 3600 && m.Latencia <=16 {
+		return true 
+	} else { 
+		return false }
 }
--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
-	"silicon/hardware"
+	"silicon/hardware" 
+	"encoding/json"
+	"os"
 )
 
 func main() {
 	var lote []hardware.Memoria
+	arquivo, err := os.ReadFile("estoque.json")
+
+    if err == nil {
+        json.Unmarshal(arquivo, &lote)
+        fmt.Println("Backup carregado! Itens recuperados: ", len(lote))
+    } else {
+        fmt.Println("Nenhum backup anterior, Começando do zero.")
+    }
 	for {
 		var opcao int
 		fmt.Println("1. Cadastrar Memória")
 		fmt.Println("2. Listar e Testar Lote")
-		fmt.Println("9. Sair")
+		fmt.Println("3. Importar para JSON")
+		fmt.Println("4. Sair")
 		fmt.Print("Escolha: ")
 		fmt.Scan(&opcao)
 
@@ -24,11 +35,34 @@ func main() {
 			fmt.Scan(&m)
 			fmt.Print("Digite a Frequencia: ")
 			fmt.Scan(&f)
+
+			if f <= 1800 {
+			    fmt.Println("Frequência não pode ser menor que 1800mhz")
+				continue }
+			if f > 6000 {
+				fmt.Println("Nenhuma DDR4 chega nisso.") 
+				continue }
+
 			fmt.Print("Digite a Latencia: ")
 			fmt.Scan(&l)
+
+			if l <= 10 {
+				fmt.Println("Essa Latencia não existe.")
+				continue }
+			if l > 22 {
+				fmt.Println("Essa Latencia não existe.")
+				continue }	
+
 			fmt.Print("Digite a Voltagem: ")
 			fmt.Scan(&v)
 
+            if v >= 2.0 {
+				fmt.Println("Essa Voltagem não existe.")
+			    continue }
+			if v <= 0.8 {
+				fmt.Println("Essa Voltagem não existe.")
+				continue}
+			
 			lote = append(lote, hardware.Memoria{
 				Marca:      m,
 				Frequencia: f,
@@ -47,8 +81,17 @@ func main() {
 				}
 			}
 		}
-
-		if opcao == 9 {
+        if opcao == 3 {
+			fmt.Println("Converter para JSON.")
+			bytes, err := json.MarshalIndent(lote, "", "  ")
+			errArquivo := os.WriteFile("estoque.json", bytes, 0644)
+			if err != nil {
+                fmt.Println("Erro ao salvar arquivo:", errArquivo)
+            } else {
+                fmt.Println("Sucesso! Arquivo 'estoque.json' criado.")
+			}
+		}
+		if opcao == 4 {
 			fmt.Println("Saindo...")
 			break
 		}
